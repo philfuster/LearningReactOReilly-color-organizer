7dddc227475300eb28fa1cdc04c03d8583dcc597
PhilFuster <pafus23@protonmail.com>
2020-07-17 22:37:13 -0400

    First iteration of color-organizer. Thought I was close before but not the case lol

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
 import colorData from './data/color-data.json';
 import ColorList from './components/ColorList.js';
+import AddColorForm from './components/AddColorForm';
+import { v4 } from 'uuid';
 import './App.css';
 
 function App() {
   const [colors, setColors] = useState(colorData);
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        const newColors = colors.map((color) =>
-          color.id === id ? { ...color, rating } : color
-        );
-        setColors(newColors);
-      }}
-      onRemoveColor={(id) => {
-        const newColors = colors.filter((color) => color.id !== id);
-        setColors(newColors);
-      }}
-    ></ColorList>
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [...colors, { id: v4(), rating: 0, title, color }];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map((color) =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+        }}
+        onRemoveColor={(id) => {
+          const newColors = colors.filter((color) => color.id !== id);
+          setColors(newColors);
+        }}
+      ></ColorList>
+    </>
   );
 }
 
--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,21 +1,24 @@
-import React, { useRef } from 'react';
-
+import React, { useState } from 'react';
+import { useInput } from './hooks/useInput';
 export default function AddColorForm({ onNewColor = (f) => f }) {
-  const txtTitle = useRef();
-  const hexColor = useRef();
+  const [titleProps, resetTitle] = useInput('');
+  const [colorProps, resetColor] = useInput('#000000');
 
   const submit = (e) => {
     e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
-    onNewColor(title, color);
-    txtTitle.current.value = '';
-    hexColor.current.value = '';
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type='text' placeHolder='color title...' required />
-      <input ref={hexColor} type='color' required />
+      <input
+        {...titleProps}
+        type='text'
+        placeholder='color title...'
+        required
+      />
+      <input {...colorProps} type='color' required />
       <button>ADD</button>
     </form>
   );
