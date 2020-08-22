cf21a48672e06a52c0ad559feec8f8ef62fab259
PhilFuster <pafus23@protonmail.com>
2020-08-22 13:04:02 -0400

    Comments for useInput hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 
+/**
+ * This hook takes an initialValue as a parameter and creates state
+ * for an input element.
+ * It return an array containing an object that has the value from state, and an onChange event handler which
+ * will call the setValue to change state of the input based on the even target value.
+ * and a second index containing a function that will reset input value to the initial
+ * value passed
+ *
+ * @param {*} initialValue - Initial value for input
+ */
 export const useInput = (initialValue) => {
   const [value, setValue] = useState(initialValue);
   return [
